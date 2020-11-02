d6f5ce55d5df4f12c76c6f6622ce73c0f7d4c2f5
jamesjr19 <ruwartja@gmail.com>
2020-11-01 20:40:07 -0500

    Hackerrank Challenge - Breaking Records

---
+++ Practice-HackerrankChallenge1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice_HackerrankChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 10, 24, 14, 4, 25, 2, 26 };
+            Console.WriteLine(string.Join(" ", breakingRecords(scores)));
+
+        }
+            static int[] breakingRecords(int[] scores)
+            {
+                int[] result = new int[2];
+
+                int countMax = 0, countMin = 0, max = scores[0], min = scores[0];
+                for (int i = 1; i < scores.Length; i++)
+                {
+                    if (scores[i] > max) { max = scores[i]; countMax++; }
+                    else if (scores[i] < min) { min = scores[i]; countMin++; }
+                }
+                result[0] = countMax; result[1] = countMin;
+            
+                return result;
+            }
+        }
+    }
+
